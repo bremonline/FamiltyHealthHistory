e01d2a3ef9ae154b7199014f70762207cd293080
bremonline <Lawrence.Brem@gmail.com>
2022-04-25 12:15:27 -0400

    Trying to find javascript file

--- source/js/hello.js
+++ source/js/hello.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
   // executes when HTML-Document is loaded and DOM is ready
   alert("document is ready");
+
+  $.getJSON( "sampledata/lawrence_brem.json", function (data) {
+    alert("json is read");
+    console.log(data);
+  });
+
+  
 });
